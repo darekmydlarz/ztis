30d113ab7ed17d2676b3d0c14d55cbfe34f65399
Dario <dario@debian>
2014-06-09 17:09:27 +0200

    revert

--- hello.py
+++ hello.py
@@ -60,12 +60,7 @@ class Consume(Resource):
 		request = requests.post("http://immense-refuge-2812.herokuapp.com/sample/test?config=2&timespan=10")
 		host = [{"host" : "flask-ztis.herokuapp.com", "path" : "/consume"}]
 		headers = {'content-type': 'application/json'}
-
-		s = requests.session()
-		s.keep_alive = False
-
 		request = s.post(generator_url, data=json.dumps(host), headers=headers)
-		request.connection.close()
 		return {}, 200
 
 
